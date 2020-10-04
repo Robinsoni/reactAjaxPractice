36ae92d8e9804742ee4a10bf7585e520b65cebb0
robin <sonirobin369@gmail.com>
2020-10-04 22:26:35 +0530

    manuplating the post and using axios

--- src/containers/Blog/Blog.js
+++ src/containers/Blog/Blog.js
@@ -1,18 +1,41 @@
 import React, { Component } from 'react';
-
+import axios from 'axios';
 import Post from '../../components/Post/Post';
 import FullPost from '../../components/FullPost/FullPost';
 import NewPost from '../../components/NewPost/NewPost';
 import './Blog.css';
 
 class Blog extends Component {
+
+    state = {
+        posts:[],
+    }
+    componentDidMount(){
+        axios.get('https://jsonplaceholder.typicode.com/posts')
+            .then(Response => {
+                console.log(Response)
+                const posts = Response.data.splice(0,4)
+                const updatedPosts = posts.map(post => (
+                    {
+                        ...post,
+                        author:'Robin'
+                    }
+                ))
+                this.setState({posts:updatedPosts})
+            })
+    
+
+    }
     render () {
+       const post =  this.state.posts.map(post => {
+           //console.log(post.title); return
+            return <Post key={post.id} title = {post.title} author = {post.author}/>
+        })
+        console.log(this.state.posts)
         return (
             <div>
                 <section className="Posts">
-                    <Post />
-                    <Post />
-                    <Post />
+                   {post}
                 </section>
                 <section>
                     <FullPost />
